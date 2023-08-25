7b030315d3a0e24064156ad80dcb95602d39e0dd
VenuMyakam <venumyakam@gmail.com>
2023-08-25 16:03:44 +0530

    gg

---
+++ Today/Backups/single-careers.php
@@ -0,0 +1,117 @@
+<?php
+/*
+Template Name: Careers page layout
+Template Post Type: post, page
+*/
+get_header(); ?>
+
+<!-- current-start -->
+<section class="curent-bg menu-mar-sp">
+    <img src="https://www.tempestadvertising.com/wp-content/uploads/2020/02/current-img-1.jpg" alt="" />
+
+    <div class="cup-tl">
+        <div class="con-bg">
+            <div class="container mb-pl-pr">
+                <div class="row">
+                    <div class="con-bg-over"></div>
+                    <div class="cun-pad">
+                        <div class="col-md-12 col-xs-12 col-sm-12 px-8">
+                            <h3>Join Our Team</h3>
+                            <p><!-- At Tempest, madness is music, rush is the order of the day, normal is boring and deadlines are breakfast. For over 20 years, we have been in the business of making businesses work better. We don’t sell to buyers. We advertise to make people’s lives better. We convert them into brand loyalists. We take people by the arm and make them see the difference between our brand and everything else. -->Tempest
+                                offers a high-energy workplace for advertising professionals to anchor themselves and
+                                build their careers - with opportunities to learn and grow. We believe in loyalty,
+                                commitment, respect and equal opportunity.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="openings">
+    <div class="container mb-pl-pr">
+        <div class="row">
+            <div class="px-8">
+                <div class="col-md-12 text-left">
+                    <h1 title="Career at Tempest">Current Openings</h1>
+                </div>
+
+
+
+
+                <div class="col-md-4 col-xs-12 col-sm-4 tab-pad mob-pad">
+                    <div class="outer">
+                        <div id="wrapper">
+                            <li><a target="_blank"
+                                    href="https://www.tempestadvertising.com/currentopenings/account-manager/"
+                                    rel="noopener">Account Manager <br>
+                                    <span class="number">Pune </span> </a>
+                            </li>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-xs-12 col-sm-4 tab-pad mob-pad ">
+                    <div class="outer">
+                        <div id="wrapper">
+                            <li><a href="https://www.tempestadvertising.com/currentopenings/campaign-manager/">Campaign
+                                    Manager <br>
+                                    <span class="number">Pune</span> </a>
+                            </li>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-xs-12 col-sm-4 tab-pad mob-pad ">
+                    <div class="outer">
+                        <div id="wrapper">
+                            <li><a href="https://www.tempestadvertising.com/currentopenings/jr-art-director/">Jr Art
+                                    Director <br>
+                                    <span class="number">Bangalore</span> </a>
+                            </li>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-xs-12 col-sm-4 tab-pad mob-pad ">
+                    <div class="outer">
+                        <div id="wrapper">
+                            <li><a href="https://www.tempestadvertising.com/currentopenings/website-designer/">Website
+                                    Designer <br>
+                                    <span class="number">Hyderabad</span> </a>
+                            </li>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-md-4 col-xs-12 col-sm-4 tab-pad mob-pad ">
+                    <div class="outer">
+                        <div id="wrapper">
+                            <li><a href="https://www.tempestadvertising.com/currentopenings/internships/">Internships<br>
+                                    <span class="number">All Branches</span> </a>
+                            </li>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-12 col-sm-4 tab-pad mob-pad ">
+                    <div class="outer">
+                        <div id="wrapper">
+                            <li><a href="https://www.tempestadvertising.com/currentopenings/web-designer/">General
+                                    Application<br>
+                                    <span class="number">All Branches</span> </a>
+                            </li>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
